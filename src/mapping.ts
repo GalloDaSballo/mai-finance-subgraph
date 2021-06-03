66d91ef49d865b9f04f5d991f8847ce182c3cb3b
LaoZi <81726233+0xlaozi@users.noreply.github.com>
2021-06-03 17:03:58 -0500

    update collateralRatio function (when there's no debt)

--- src/mapping.ts
+++ src/mapping.ts
@@ -50,7 +50,7 @@ export function handleDepositCollateral(event: DepositCollateral): void {
   if(vault.borrowed.gt(BigInt.fromI32(0))){
     vault.collateralRatio = vault.deposited.toBigDecimal().div(vault.borrowed.toBigDecimal())
   } else {
-    vault.collateralRatio = vault.deposited.toBigDecimal()
+    vault.collateralRatio = 0
   }
   
   vault.save()
@@ -66,7 +66,7 @@ export function handleWithdrawCollateral(event: WithdrawCollateral): void {
   if(vault.borrowed.gt(BigInt.fromI32(0))){
     vault.collateralRatio = vault.deposited.toBigDecimal().div(vault.borrowed.toBigDecimal())
   } else {
-    vault.collateralRatio = vault.deposited.toBigDecimal()
+    vault.collateralRatio = 0
   }
 
   vault.save()
@@ -81,7 +81,7 @@ export function handleBorrowToken(event: BorrowToken): void {
   if(vault.borrowed.gt(BigInt.fromI32(0))){
     vault.collateralRatio = vault.deposited.toBigDecimal().div(vault.borrowed.toBigDecimal())
   } else {
-    vault.collateralRatio = vault.deposited.toBigDecimal()
+    vault.collateralRatio = 0
   }
 
   vault.save()
@@ -101,7 +101,7 @@ export function handlePayBackToken(event: PayBackToken): void {
   if(vault.borrowed.gt(BigInt.fromI32(0))){
     vault.collateralRatio = vault.deposited.toBigDecimal().div(vault.borrowed.toBigDecimal())
   } else {
-    vault.collateralRatio = vault.deposited.toBigDecimal()
+    vault.collateralRatio = 0
   }
 
   vault.save()
@@ -134,7 +134,7 @@ export function handleBuyRiskyVault(event: BuyRiskyVault): void {
   if(vault.borrowed.gt(BigInt.fromI32(0))){
     vault.collateralRatio = vault.deposited.toBigDecimal().div(vault.borrowed.toBigDecimal())
   } else {
-    vault.collateralRatio = vault.deposited.toBigDecimal()
+    vault.collateralRatio = 0
   }
   vault.save()
 }
